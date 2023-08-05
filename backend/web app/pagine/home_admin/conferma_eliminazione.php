9f91911f15285a79819740f6868b7290039f1384
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-08-05 10:24:46 +0200

    add delete events system

--- backend/web app/pagine/home_admin/conferma_eliminazione.php
+++ backend/web app/pagine/home_admin/conferma_eliminazione.php
@@ -7,7 +7,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel = "stylesheet" href = "../../style/style3.css">
+    <link rel = "stylesheet" href = "../../style/style_conf.css">
 </head>
 <body>
     <?php
@@ -24,19 +24,19 @@
                 if (isset($_POST['codice'])) {
                     $codice = $_POST['codice'];
 
-                    echo "<h3>Clicca su 'Conferma cancellazione' per confermare l'eliminazione dell'evento (TUTTI i dati e le modifiche andranno persi). Se vuoi tornare alla pagina precedente clicca su 'Indietro'</h3>";
+                    echo "<h3>Clicca su 'Conferma cancellazione' per confermare l'eliminazione dell'evento (TUTTI i dati e le modifiche andranno persi). Se vuoi tornare alla pagina precedente clicca su 'Indietro'</h3><br>";
                     echo "<form class = 'form-group' action = '../../script/gestione_eventi/elimina_evento.php' method = 'POST'>";
                     echo "<input type = 'hidden' id = 'codice' name = 'codice' value = '".$codice."' />";
-                    echo "<input type = 'submit' class = 'btn btn-danger's value = 'Conferma cancellazione' />";
+                    echo "<input type = 'submit' class = 'btn btn-danger' value = 'Conferma cancellazione' />";
                     echo "</form>";
-                    echo "<button type = 'submit' class = 'btn btn-success'>";
-                    echo "Indietro";
-                    echo "</button>";
+                    echo "<form class = 'form-group' action = 'home_admin.php'>";
+                    echo "<input type = 'submit' class = 'btn btn-success' value = 'Indietro' />";
+                    echo "</form>";
                 } else {
-                    echo "<h3>Errore nella ricerca dell'evento da eliminare</h3>";
-                    echo "<button type = 'submit' class = 'btn btn-success'>";
-                    echo "Indietro";
-                    echo "</button>";
+                    echo "<h3>Errore nella ricerca dell'evento da eliminare</h3><br>";
+                    echo "<form class = 'form-group' action = 'home_admin.php'>";
+                    echo "<input type = 'submit' class = 'btn btn-success' value = 'Indietro' />";
+                    echo "</form>";
                 }
             ?>
 
---
+++ backend/web app/script/gestione_eventi/elimina_evento.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+
+    if (isset($_POST['codice'])) {
+        include_once('../connection.php');
+        $codice = $_POST['codice'];
+
+        $sql = "DELETE FROM eventi.eventi AS e WHERE e.codice = $1";
+        $res = pg_prepare($connection, "", $sql);
+        $res = pg_execute($connection, "", array($codice));
+
+        if (!$res) {
+            $_SESSION['eliminazione'] = "Errore nell'eliminazione dell'evento.";
+            header('Location: ../../pagine/home_admin/conf_eliminazione.php');
+            exit();
+        } else {
+            $_SESSION['eliminazione'] = "Eliminazione dell'evento avvenuta con successo!";
+            header('Location: ../../pagine/home_admin/conf_eliminazione.php');
+            exit();
+        }
+    } else {
+        $_SESSION['eliminazione'] = "Errore nell'eliminazione dell'evento: codice non trovato.";
+        header('Location: ../../pagine/home_admin/conf_eliminazione.php');
+        exit();
+    }
+?>
