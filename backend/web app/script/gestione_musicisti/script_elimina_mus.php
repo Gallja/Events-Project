97ce30b7348b2c37360dbc44bad07befd92f06b7
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-19 19:46:03 +0200

    fix check login

--- backend/web app/script/gestione_musicisti/script_elimina_mus.php
+++ backend/web app/script/gestione_musicisti/script_elimina_mus.php
@@ -5,14 +5,16 @@
         include_once('../management/connection.php');
         $id = $_POST['id'];
 
-        $sql = "DELETE FROM eventi.musicisti AS m WHERE m.id = $1";
+        $sql = "DELETE FROM eventi.musicisti AS m WHERE m.id_musicista = $1";
         $res = pg_prepare($connection, "ris", $sql);
         $res = pg_execute($connection, "ris", array($id));
 
         if (!$res) {
             $_SESSION['eliminazione_musicista'] = "Errore nell'eliminazione del musicista.";
+            echo pg_last_error($connection)."\n";
             header('Location: ../../pagine/home_admin/musicisti/conf_elim_mus2.php');
             exit();
+            
         } else {
             $_SESSION['eliminazione_musicista'] = "Eliminazione del musicista avvenuta con successo!";
             header('Location: ../../pagine/home_admin/musicisti/conf_elim_mus2.php');
--- backend/web app/script/management/check_not_login.php
+++ backend/web app/script/management/check_not_login.php
@@ -2,7 +2,7 @@
     session_start();
 
     if (isset($_SESSION['isLogin'])) {
-        header('Location: ../../pagine/home_admin/eventi/home_admin.php');
+        header('Location: ../pagine/home_admin/eventi/home_admin.php');
         exit();
     }
 ?>
