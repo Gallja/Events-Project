5b4c718a42f7786df843a7bf7fe30fc7f6791e47
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-19 17:07:21 +0200

    fix path in navbar

--- backend/web app/pagine/home_admin/comici/insert_comico.php
+++ backend/web app/pagine/home_admin/comici/insert_comico.php
@@ -26,7 +26,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item active">
-                                <a class="nav-link" href="../home_admin.php">Home <span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="../eventi/home_admin.php">Home <span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="insert_comico.php">Comici</a>
