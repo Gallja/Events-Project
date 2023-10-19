a75a50517454e2dbc936b73e1fdd10ca6f940048
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-19 19:49:25 +0200

    update API

--- API_events.php
+++ API_events.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-// open connection to the DB
+// open connection to DB
 include_once('backend\web app\script\management\connection.php');
 
 // API request - only read events
@@ -9,7 +9,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // endpoint
     $result = pg_query($connection, "SELECT * FROM eventi.eventi");
     $eventi = pg_fetch_all($result);
-    echo json_encode($eventi);
+
+    $result2 = pg_query($connection, "SELECT * FROM eventi.comici");
+    $comici = pg_fetch_all($result2);
+
+    $result3 = pg_query($connection, "SELECT * FROM eventi.musicisti");
+    $musicisti = pg_fetch_all($result3);
+
+    echo json_encode($eventi).json_encode($comici).json_encode($musicisti);
 } 
 
 // close connection to the DB
