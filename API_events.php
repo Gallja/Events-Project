8f671b9509ab7c4f090da1b8fde4614c9e02876c
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-23 21:27:30 +0200

    fix API

--- API_events.php
+++ API_events.php
@@ -16,7 +16,12 @@
         $result3 = pg_query($connection, "SELECT * FROM eventi.musicisti");
         $musicisti = pg_fetch_all($result3);
 
-        echo json_encode($eventi).json_encode($comici).json_encode($musicisti);
+        $response['eventi'] = $eventi;
+        $response['comici'] = $comici;
+        $rensonse['musicisti'] = $musicisti;
+
+        // JSON encoding
+        echo json_encode($response);
     } 
 
     // close connection to the DB
