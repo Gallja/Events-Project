977f5b76ee61a1fc087874a2c4839095e77ce8bb
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-27 13:57:18 +0200

    update navabr backend

--- backend/web app/pagine/home_admin/eventi/modifica_evento_page.php
+++ backend/web app/pagine/home_admin/eventi/modifica_evento_page.php
@@ -48,7 +48,7 @@
                     </li>
                 </ul>
                 <div class="row justify-content-center align-items-center" id = "div_search">
-                    <input id = "input_ricerca" class="form-control mr-sm-2" type="search" placeholder="Cerca" aria-label="Search">
+                    <input id = "input_ricerca" class="form-control mr-sm-2" type="search" placeholder="Cerca campo" aria-label="Search">
                     <button onclick = "ricerca()" id = "input_bottone" class="btn btn-outline-success my-2 my-sm-0">Cerca</button>
                 </div>
             </div>
