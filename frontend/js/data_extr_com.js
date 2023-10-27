15c7408a9163ea4ad069def34e4295aee043c22a
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-27 11:25:21 +0200

    fix comici page in frontend

--- frontend/js/data_extr_com.js
+++ frontend/js/data_extr_com.js
@@ -14,51 +14,22 @@ function fetchData() {
 }
 
 function displayComici(comici) {
-    const comiciContainer = document.getElementById('carouselExampleIndicators');
+    const comiciContainer = document.getElementById('contComici');
     comiciContainer.innerHTML = '';
-
-    const indicatori = document.createElement('ol');
-    indicatori.setAttribute('class', 'carousel-indicators');
-
-    comici.forEach((comico, index) => {
-        const indicator = document.createElement('li');
-        indicator.setAttribute('data-target', '#carouselExampleIndicators');
-        indicator.setAttribute('data-slide-to', index);
-
-        if (index === 0) {
-            indicator.classList.add('active');
-        }
-
-        indicatori.appendChild(indicator);
-
+    comici.forEach(comico => {
         const comicoDiv = document.createElement('div');
-        comicoDiv.setAttribute('class', 'carousel-item');
-
-        if (index === 0) {
-            comicoDiv.classList.add('active');
-        }
-
+        comicoDiv.setAttribute('class', 'home-full-width-banner section container');
+        const nomeMaiusc = comico.nome_comico.toUpperCase();
+        const cognomeMaiusc = comico.cognome_comico.toUpperCase();
         comicoDiv.innerHTML = `
-            <img class="d-block w-100" src="data:image/jpeg;base64,${comico.immagine}" alt="${comico.nome_comico} ${comico.cognome_comico}" id="caroselloCom">
-            <div class="carousel-caption">
-                <h5>${comico.nome_comico} ${comico.cognome_comico}</h5>
-                <p>${comico.bio}</p>
+            <div class="home-left1" id="third">
+                <div class="home-content">
+                    <span class="home-text29">${nomeMaiusc} ${cognomeMaiusc}</span>
+                    <span class="home-text30">${comico.bio}</span>
+                </div>
             </div>
+            <img src="data:image/jpg;base64,${comico.immagine}" class="home-image6">
         `;
-
         comiciContainer.appendChild(comicoDiv);
     });
-
-    comiciContainer.appendChild(indicatori);
-
-    comiciContainer.innerHTML += `
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        `;
 }
