abaf721470accdc9143163c76dc4a42b4cf9e74d
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-28 11:45:50 +0200

    udpate API

--- API_events.php
+++ API_events.php
@@ -7,6 +7,36 @@
     // API request - only read events
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         // endpoint
+
+        if (isset($_GET['evento_id'])) {
+            $evento_id = $_GET['evento_id'];
+
+            $result_comici = pg_query($connection, "SELECT c.nome_evento, e.data_evento, e.ora_evento, e.luogo, e.descrizione, e.link,
+                                                           c.nome_comico, c.cognome_comico, encode(c.profilo, 'base64') AS immagine
+                                                    FROM eventi.comici AS c 
+                                                    INNER JOIN eventi.eventi_comici AS ec
+                                                    ON c.id = ec.comico
+                                                    INNER JOIN eventi.eventi AS e
+                                                    ON ec.evento = e.codice
+                                                    WHERE ec.evento = $evento_id");
+
+            $eventi_comici = pg_fetch_all($result_comici);
+
+            $result_musicisti = pg_query($connection, "SELECT e.nome_evento, e.data_evento, e.ora_evento, e.luogo, e.descrizione, e.link,
+                                                              m.nome_musicista, encode(m.profilo_musicista, 'base64') AS immagine
+                                                       FROM eventi.musicisti AS m
+                                                       INNER JOIN eventi.eventi_musicisti AS em
+                                                       ON m.id_musicista = em.musicista
+                                                       INNER JOIN eventi.eventi AS e
+                                                       ON em.evento = e.codice
+                                                       WHERE em.evento = $evento_id");
+
+            $eventi_musicisti = pg_fetch_all($result_musicisti);
+
+            $response['eventi_comici'] = $eventi_comici;
+            $response['eventi_musicisti'] = $eventi_musicisti;
+        }
+
         $result = pg_query($connection, "SELECT e.codice, e.nome_evento, e.data_evento, 
                                                 e.luogo, encode(e.immagine, 'base64') AS immagine, e.descrizione
                                          FROM eventi.eventi AS e
@@ -21,23 +51,15 @@
                                           FROM eventi.musicisti AS m");
         $musicisti = pg_fetch_all($result3);
 
-        $result4 = pg_query($connection, "SELECT * FROM eventi.eventi_comici");
-        $eventi_comici = pg_fetch_all($result4);
-
-        $result5 = pg_query($connection, "SELECT * FROM eventi.eventi_musicisti");
-        $eventi_musicisti = pg_fetch_all($result5);
-
-        $result6 = pg_query($connection, "SELECT e.codice, e.nome_evento, e.data_evento,
+        $result4 = pg_query($connection, "SELECT e.codice, e.nome_evento, e.data_evento,
                                                  e.luogo, encode(e.immagine, 'base64') AS immagine, e.descrizione
                                           FROM eventi.eventi AS e
                                           WHERE e.data_evento < CURRENT_DATE");
-        $archivio_eventi = pg_fetch_all($result6);
+        $archivio_eventi = pg_fetch_all($result4);
 
         $response['eventi'] = $eventi;
         $response['comici'] = $comici;
         $response['musicisti'] = $musicisti;
-        $response['eventi_comici'] = $eventi_comici;
-        $response['eventi_musicisti'] = $eventi_musicisti;
         $response['archivio_eventi'] = $archivio_eventi;
 
         // JSON encoding
