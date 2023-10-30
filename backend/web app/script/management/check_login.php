17ba50251334e57c5db2b58be971e6065d1483b4
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-10-30 12:49:00 +0100

    update new branch

--- backend/web app/script/management/check_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    if (!isset($_SESSION['isLogin'])) {
-        header('Location: ../../../pagine/home.php');
-        exit();
-    }
-?>
--- backend/web app/script/management/check_not_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    if (isset($_SESSION['isLogin'])) {
-        header('Location: ../pagine/home_admin/eventi/home_admin.php');
-        exit();
-    }
-?>
